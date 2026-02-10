7f7a550fa09a4870edc1fc1db841b800c4d8164a
Nirvana638 <143329759+Nirvana638@users.noreply.github.com>
2026-02-10 18:38:04 +0800

    Update test.c

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -20,7 +20,7 @@ static int test_pass = 0;
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
-static void test_parse_null() {
+static void test_parse_null(void) {
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
@@ -80,7 +80,7 @@ static void test_parse() {
     test_parse_root_not_singular();
 }
 
-int main() {
+int main(void) {
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
