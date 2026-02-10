62cd757478cefe69442cd1cec3cb1e603d408861
Nirvana638 <143329759+Nirvana638@users.noreply.github.com>
2026-02-10 17:48:14 +0800

    Update test.c

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -23,6 +23,9 @@ static int test_pass = 0;
 static void test_parse_null() {
     lept_value v;
     v.type = LEPT_FALSE;
+    printf("LEPT_NULL = %d\n", LEPT_NULL);
+    printf("lept_get_type before parse = %d\n", lept_get_type(&v));
+   
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
